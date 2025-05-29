a4a978f686a89ce4e504dcc560187dbf6adad94b
shohid-okobiz <dev.okobiz@gmail.com>
2025-05-29 19:03:02 +0600

    updted

--- src/modules/land/land.services.ts
+++ src/modules/land/land.services.ts
@@ -255,20 +255,7 @@ const LandServices = {
     }
 
   },
-  // searchLandListingHandleMethod: async (payload: any) => {
-  //   try {
-  //     const result = await findAllSearchingLand(payload)
-  //     return result;
-
-  //   } catch (error) {
-  //     if (error instanceof Error) {
-  //       throw error;
-  //     } else {
-  //       throw new Error('Unknown Error Occurred In Retrieve One Listed Land Service');
-  //     }
-  //   }
-
-  // }
+  
 };
 
 export default LandServices;
